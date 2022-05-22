2fd8a9302df0cd3399d2ea305a0c8ffa73c55cb1
326eunjin <326eunjin@naver.com>
2022-05-22 13:30:40 +0900

    frontend

---
+++ bob42/frontend/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {
+        posts: []
+    };
+
+    async componentDidMount() {
+        try {
+            const res = await fetch('http://127.0.0.1:8000/api/');
+            const posts = await res.json();
+            this.setState({
+                posts
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.posts.map(item => (
+                    <div key={item.iduser}>
+                        <h1>{item.name}</h1>
+                        <span>{item.phone}</span>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
+
+export default App;
